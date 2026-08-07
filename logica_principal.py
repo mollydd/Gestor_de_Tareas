5d69329d59117479cc5b8f4ffada717f269ad895
Paula Machacon <paulamachacon8@gmail.com>
2026-08-07 10:45:33 -0500

    feat: agregar actualizacion de tareas

--- logica_principal.py
+++ logica_principal.py
@@ -23,13 +23,19 @@ def agregar_tarea():
     almacenador_tareas.append(tarea)
 
 def Auxiliar_buscar_por_id(id):
-    while True:
         id_pedir=int(input(id))
         for i in almacenador_tareas:
             if i.id ==  id_pedir:
-                return i.titulo
+                return i
         return None
 
+def buscar_tarea():
+    Tarea_encontrada=Auxiliar_buscar_por_id("ingrese el id correspondiente a la tarea que desea buscar: ")
+    if Tarea_encontrada is None:
+        print("lo sentimos, no hay una tarea registrada con ese id")
+    else:
+        return id.mostrar_datos()
+    
 def mostrar_tareas():
     if not almacenador_tareas:
         print("lo sentimos, no hay tareas guardadas en este momento")
@@ -44,6 +50,28 @@ def guardar_tarea():
     with open("tareas.json" , "w") as archivo:
         json.dump(datos,archivo)
 
+def actualizar_tareas():
+    tarea_encontrada= Auxiliar_buscar_por_id("ingrese el id correspondiente a la tarea que desea actualizar: ")
+    if tarea_encontrada is None:
+        print("no hay una tarea con ese id en estos momentos0")
+        return
+
+    print("Tarea encontrado")
+
+    datos= int(input("que datos desea actualizar? escriba el numero d la opcion q desea cambiar: \n1. Titulo \n2.Descripcion \n3.Estado \n4.Prioridad"))
+    if datos == 1:
+        tarea_encontrada.titulo = input("ingrese el nuevo nombre de la tarea: ")
+    elif datos == 2:
+        tarea_encontrada.descripcion=input("ingrese la nueva descripcion de la tarea: ")
+    elif datos == 3:
+        tarea_encontrada.estado=input("ingrese el nuevo estado de la tarea: ")
+    elif datos == 4:
+        tarea_encontrada.prioridad=input("ingrese la nueva prioridad de la tarea: ")
+    else:
+        print("opcion invalida, ingrese nuevamente")
+
+    guardar_tarea()
+    
 "este sirve para cargar los datos"
 def cargar_tareas():
     with open("tareas.json" , "r") as archivo:
